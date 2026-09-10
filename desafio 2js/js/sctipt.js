8c529388630af88d050d87109d3786f888973af9
Isabella-BG <ana.berrios2027@frech.superate.org>
2026-09-10 09:48:54 -0600

    desafio 2js
    finalizado

---
+++ desafio 2js/js/sctipt.js
@@ -0,0 +1,15 @@
+function neon() {
+document.getElementById("frasemotivadora").classList.remove("outline-text");
+document.getElementById("frasemotivadora").classList.remove("retro-shadow");
+document.getElementById("frasemotivadora").classList.add("neon-text");
+}
+function outline() {
+document.getElementById("frasemotivadora").classList.remove("retro-shadow");
+document.getElementById("frasemotivadora").classList.remove("neon-text");
+document.getElementById("frasemotivadora").classList.add("outline-text");
+}
+function retro() {
+document.getElementById("frasemotivadora").classList.remove("outline-text");
+document.getElementById("frasemotivadora").classList.remove("neon-text");
+document.getElementById("frasemotivadora").classList.add("retro-shadow");
+}
